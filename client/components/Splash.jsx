bab20bd1cedb88f440e0c46455f7a951cba8e79a
Tess Meyer <tessoliviameyer@gmail.com>
2019-08-13 15:35:29 +1200

    mobile splash page looking good

--- client/components/Splash.jsx
+++ client/components/Splash.jsx
@@ -4,14 +4,19 @@ class Splash extends React.Component {
     render() { 
         return ( 
             <div className='splash'>
-                <p className="splash-text">Hi! I'm Tess</p>
-                    <a href="https://www.linkedin.com/in/tess-meyer-6b7a2a160">
-                        <img className="me" src="/me.jpeg" alt=""/>
-                    </a>
-                <p className="splash-text">I'm a developer</p>
+         
+                <div className="splash-text">
+                    <h1>I<span className="green">'</span>m Tess Meyer<span className="green">.</span></h1>
+                    <hr/>
+                    <h1>Web Developer <span className="green">&amp;</span> Problem Solver</h1>
+                </div>
             </div>
          );
     }
 }
  
+      {/* <a href="https://www.linkedin.com/in/tess-meyer-6b7a2a160">
+                        <img className="me" src="/me.jpeg" alt=""/>
+                    </a>
+                <p className="splash-text">I'm a developer</p> */}
 export default Splash;
--- client/components/Technologies.jsx
+++ client/components/Technologies.jsx
@@ -5,32 +5,8 @@ class Technologies extends React.Component {
         return ( 
             <div className='technologies'>
                 <h2>These are the technologies I know</h2>
-                <h3>Node.JS</h3>
-                <p>
-                    Af la croix lo-fi, retro hashtag tattooed waistcoat shaman sartorial
-                    marfa chambray craft beer keffiyeh. 8-bit fingerstache fashion axe
-                    shaman pour-over, organic irony. Church-key vaporware yr normcore. 
-                </p>
-                <h3>React</h3>
-                <p>
-                    Shoreditch art party intelligentsia crucifix. Pickled four dollar 
-                    toast kogi DIY crucifix disrupt. Air plant tbh etsy poke kogi cloud
-                    bread knausgaard church-key yr hoodie narwhal cold-pressed photo
-                    booth pabst polaroid.
-                </p>
-                <h3>SCSS</h3>
-                <p>
-                    Af la croix lo-fi, retro hashtag tattooed waistcoat shaman sartorial
-                    marfa chambray craft beer keffiyeh. 8-bit fingerstache fashion axe
-                    shaman pour-over, organic irony. Church-key vaporware yr normcore. 
-                </p>
-                <h3>HTML5</h3>
-                 <p>
-                    Shoreditch art party intelligentsia crucifix. Pickled four dollar 
-                    toast kogi DIY crucifix disrupt. Air plant tbh etsy poke kogi cloud
-                    bread knausgaard church-key yr hoodie narwhal cold-pressed photo
-                    booth pabst polaroid.
-                </p>
+            <span>HTML</span>
+
             </div>
             
          );
